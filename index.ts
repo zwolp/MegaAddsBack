bc92831ac0bbab6f9c05180bf9ceb48d066e278a
Patryk Zwolak <patrykzwolak.pz@gmail.com>
2023-02-08 11:22:27 +0100

    added rate limiter

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import express, { json } from "express";
 import cors from "cors";
 import 'express-async-errors';
+import rateLimit from 'express-rate-limit'
 import { handleError, ValidationError } from "./utils/errors";
 
 const app = express();
@@ -9,8 +10,11 @@ app.use(cors({
   origin: 'http://localhost:3000',
 }));
 app.use(json());
-
-app.use(handleError)
+app.use(handleError);
+app.use(rateLimit({
+  windowMs: 5*60*1000,
+  max: 100,
+}))
 
 app.listen(3001, '0.0.0.0', () => {
   console.log('Listening on http://localhost:3001');
